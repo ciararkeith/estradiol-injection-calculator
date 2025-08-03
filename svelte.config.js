4d186572d13c245681c31ad0d2c1aea81acff633
ciara r keith <ciararkeith@gmail.com>
2025-08-02 22:26:33 -0500

    asdasfda

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,10 +13,11 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : '/estradiol-injection-calculator',
-    },
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      base: dev ? '' : 'estradiol-injection-calculator'
+    }),
   }
 };
 
